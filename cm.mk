f22f46f314b4526e1aedd14a667e9c7dba453966
SlimRomsLegacy <droidmodderxtreme21@gmail.com>
2014-02-16 12:34:19 -0500

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,10 +1,4 @@
 $(call inherit-product, device/sony/kumquat/full_kumquat.mk)
 
-# Inherit CM common GSM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
-# Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-PRODUCT_NAME := cm_kumquat
+PRODUCT_NAME := slim_kumquat
 PRODUCT_DEVICE := kumquat
